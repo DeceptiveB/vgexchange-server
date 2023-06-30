999d9271031dee031a2b0ee8a9fb11e5f004e61e
deceptiveb <berny0996@gmail.com>
2023-06-29 19:57:26 -0500

    added properties file

---
+++ src/main/java/com/example/videogamexchange/VideogamexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.videogamexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogamexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogamexchangeApplication.class, args);
+	}
+
+}
